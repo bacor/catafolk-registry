f6104ee893d2448a4c500996ed87a00606bd225c
Bas Cornelissen <bjmcornelissen@gmail.com>
2021-10-07 23:18:09 +0200

    initial commit

---
+++ corpora/densmore-nootka/0.0.1/README.md
@@ -0,0 +1,30 @@
+
+Downloading
+-----------
+
+This dataset is part of the *Densmore Collection*, which can be downloaded from Daniel Shanahan's [GitHub repository](https://github.com/shanahdt/densmore/). 
+All files in this subset are in the [`Densmore/acoma`](https://github.com/shanahdt/densmore/tree/master/Densmore/acoma) directory.
+
+Cite as:
+
+Shanahan, D., & Shanahan, E. (n.d.). The Densmore collection of Native American songs: A new corpus for studies of effects of geograpy, language and social function on folk song. 3.
+
+Catafolk notes
+--------------
+
+- Song 49 appears twice in the dataset, with two different drum parts. 
+    Only the first version is included, so `No_49_Wand_Dance_Song_-_with_2nd_rendition_drums.krn` is
+    ignored.
+- All songs have the same location (Neah Bay, Washington), since 
+Densmore reports that she collected all songs there (p. V).
+
+
+Notes on additional metadata
+----------------------------
+
+[Google Docs sheet](https://docs.google.com/spreadsheets/d/1uzxubs3CQsWIwBBVZgmlWY7vN9xISJ1W5GNxYKsed_U)
+
+- If there was no indication of the use of a drum (e.g. a drum tempo, or a drum trancription),
+    the song is assumed to be monophonic.
+- Meters were transcribed (mostly) in order of occurance (but every meter is included only once)
+- Tempo transcriptions generally follow the tempo indication for the voice (sometimes a drum tempo is also indicated)
---
+++ corpora/densmore-nootka/0.0.1/src/index.py
@@ -0,0 +1,115 @@
+import re
+from pathlib import Path
+from catafolk.configuration import Configuration
+from catafolk.index import Entry, Index
+from catafolk.sources import CSVSource, FileSource
+import json
+
+
+class DensmoreNootkaEntry(Entry):
+
+    source_names = ["meta", "file"]
+
+    default_source = "meta"
+
+    constants = dict(
+        dataset_id="densmore-nootka",
+        file_has_music=True,
+        file_has_lyrics=True,
+        file_has_license=False,
+        collectors="Frances Densmore",
+        collection_date_earliest=1923,
+        collection_date_latest=1926,
+        encoders="Daniel Shanahan|Eva Shanahan",
+        encoding_date="2014",
+        copyright="Copyright 2014 Daniel and Eva Shanahan",
+        location="Neah Bay, Washington, U.S.A",
+        latitude=48.365556,
+        longitude=-124.615556,
+        auto_geocoded=False,
+        voice_use=True,
+        publication_key="densmore1939nootka",
+        publication_type="book",
+        publication_authors="Frances Densmore",
+        publication_date=1939,
+    )
+
+    mappings = dict(
+        file_path=("file", "cf_path"),
+        file_format=("file", "cf_format"),
+        file_checksum=("file", "cf_checksum"),
+        culture=("meta", "culture"),
+        performers=("meta", "performer"),
+        performer_genders=("meta", "performer_gender"),
+        tempo=("meta", "bpm"),
+        publication_song_num=("meta", "song_num"),
+        publication_page_num=("meta", "page_num"),
+    )
+
+    def file_url(self):
+        filename = self.get("filename")
+        return f"https://github.com/shanahdt/densmore/blob/master/Densmore/nootka/{filename}.krn"
+
+    def get_glottolog_id(self):
+        mapping = dict(Makah="maka1318", Clayoquot="nuuc1236", Quileute="quil1240")
+        culture = self.get("culture")
+        return mapping.get(culture, None)
+
+    def get_language(self):
+        mapping = dict(Quileute="Quileute", Clayoquot="Nuu-chah-nulth", Makah="Makah")
+        culture = self.get("culture")
+        return mapping.get(culture, None)
+
+    def get_publication_preview_url(self):
+        page_num = self.get("publication_page_num") + 52
+        return (
+            f"https://babel.hathitrust.org/cgi/pt?id=mdp.39015025347033&seq={page_num}"
+        )
+
+    def get_publication_song_num(self):
+        otl = self.get_from_source("file", "OTL")
+        matches = re.match("No_+(\d+)_([^\.]+).krn", otl)
+        return int(matches[1])
+
+    def get_title(self):
+        otl = self.get_from_source("file", "OTL")
+        matches = re.match("No_+(\d+)_([^\.]+).krn", otl)
+        title = matches[2].replace("_", " ")
+        return title
+
+    def get_genres(self):
+        superfunction = self.get_from_source("file", "SUPERFUNCTION")
+        if type(superfunction) == str:
+            return superfunction
+        else:
+            return "|".join(sorted(set(superfunction)))
+
+    def get_other_fields(self):
+        obj = dict(
+            language_type=self.get_from_source("file", "LANG"),
+            language_family=self.get_from_source("file", "LING_GROUP"),
+            social_function=self.get_from_source("file", "SOCIAL_FUNCTION"),
+        )
+        return json.dumps(obj)
+
+
+def generate_index(data_dir):
+    corpus_dir = Path(__file__).parent.parent
+    index = Index()
+    paths = data_dir.glob("data/*.krn")
+    for path in paths:
+        matches = re.match("No_+(\d+)", path.name)
+        entry_id = f"nootka{matches[1]:0>3}"
+        sources = dict(
+            file=FileSource(path),
+            meta=CSVSource(corpus_dir / "src/additional-metadata.csv"),
+        )
+        entry = DensmoreNootkaEntry(entry_id, sources)
+        index.add_entry(entry)
+    index.export(corpus_dir / "index.csv")
+
+
+if __name__ == "__main__":
+    config = Configuration()
+    data_dir = Path(config.get("data_dir")) / "densmore-nootka" / "0.0.1"
+    generate_index(data_dir)
