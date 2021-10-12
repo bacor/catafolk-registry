b5e6a27a01df31e7413f02d77990667e7a1fc45c
Bas Cornelissen <bjmcornelissen@gmail.com>
2021-10-12 16:14:13 +0200

    use EssenEntry

--- corpora/boehme-altdeutsches-liederbuch/0.1.0/src/index.py
+++ corpora/boehme-altdeutsches-liederbuch/0.1.0/src/index.py
@@ -1,20 +1,10 @@
-import re
 from pathlib import Path
 from catafolk.configuration import Configuration
-from catafolk.index import IndexEntry, Index
-from catafolk.sources import CSVSource, FileSource
-import json
-from catafolk.utils import is_iterable
-import yaml
-from catafolk.operations import map_values
+from catafolk.index import Index
+from catafolk.sources import FileSource
+from catafolk.corpora import EssenEntry
 
-with open(Path(__file__).parent / "genre-mapping.yml", "r") as stream:
-    GENRE_MAPPING = yaml.safe_load(stream)
-
-
-class BoehmeAltdeutschEntry(IndexEntry):
-
-    source_names = ["file"]
+class BoehmeAltdeutschEntry(EssenEntry):
 
     constants = dict(
         dataset_id="boehme-altdeutsches-liederbuch",
@@ -27,36 +17,7 @@ class BoehmeAltdeutschEntry(IndexEntry):
         publication_key="boehme1877altdeutsches",
     )
 
-    mappings = dict(
-        file_path="file.cf_path",
-        file_format="file.cf_format",
-        cf_checksum="file.cf_checksum",
-        encoders="file.EED",
-        copyright="file.YEM",
-        version="file.EEV",
-        title="file.OTL",
-        metric_classification="file.AMT",
-    )
-
-    def get_comments(self):
-        # Comments can be either strings or lists; we have to get them in one
-        # flat list...
-        comments = self.get("file._comments", [])
-        if type(comments) == str:
-            comments = [comments]
-        onb = self.get("file.ONB")
-        if type(onb) == str:
-            comments += [onb]
-        else:
-            comments.extend(onb)
-        return [c for c in comments if c is not None]
-
-    def get_genres(self):
-        agn = self.get("file.AGN")
-        if agn is None:
-            return []
-        genres = [genre.strip() for genre in agn.split()]
-        return map_values(*genres, mapping=GENRE_MAPPING, return_missing=False)
+    sct_pattern = "A0*((\\d+)(\\w+)?)"
 
     def get_file_url(self):
         id = self.get("id")
@@ -68,31 +29,9 @@ class BoehmeAltdeutschEntry(IndexEntry):
             f"https://verovio.humdrum.org/?file=essen/europa/deutschl/altdeu1/{id}.krn"
         )
 
-    def get_location(self):
-        loc = self.get("file.ARE", "")
-        return loc.replace("Europa, Mitteleuropa, Deutschland", "Germany")
-
-    def get_instrumentation(self):
-        ain = self.get("file.AIN")
-        return ain.replace("vox", "voice")
-
-    def get_catalogue_num(self):
-        sct = self.get("file.SCT")
-        matches = re.match("A0*((\\d+)(\\w+)?)", sct)
-        return matches[0]
-
-    def get_publication_song_num(self):
-        sct = self.get("file.SCT")
-        matches = re.match("A0*((\\d+)(\\w+)?)", sct)
-        return matches[1].lower()
-
-    def get_tune_family_id(self):
-        sct = self.get("file.SCT")
-        matches = re.match("A0*((\\d+)(\\w+)?)", sct)
-        return matches[2]
-
-
-def generate_index(data_dir):
+def generate_index():
+    config = Configuration()
+    data_dir = Path(config.get("data_dir")) / "boehme-altdeutsches-liederbuch" / "0.1.0"
     index = Index()
     paths = data_dir.glob("data/*.krn")
     for path in paths:
@@ -104,8 +43,5 @@ def generate_index(data_dir):
     corpus_dir = Path(__file__).parent.parent
     index.export_csv(corpus_dir / "index.csv")
 
-
 if __name__ == "__main__":
-    config = Configuration()
-    data_dir = Path(config.get("data_dir")) / "boehme-altdeutsches-liederbuch" / "0.1.0"
-    generate_index(data_dir)
+    generate_index()
