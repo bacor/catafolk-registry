180bfe4df76b5849da5bd12c23c4e80f16550eb3
bacor <mail@bascornelissen.nl>
2021-11-08 10:59:10 +0100

    initial commit

---
+++ export.py
@@ -0,0 +1,76 @@
+from pathlib import Path
+from typing import Union, List, Iterable
+import re
+import subprocess
+
+
+def iter_corpus_dir(
+    corpora_dir: Union[str, Path] = "corpora",
+    latest_only: bool = True,
+    include_corpora: List[str] = [],
+    exclude_corpora: List[str] = [],
+) -> Iterable[Path]:
+    """Iterate over all corpus directories. Either visit all corpora or only
+    the latest versions
+
+    Parameters
+    ----------
+    corpora_dir : Union[str, Path], optional
+        The directory containing all corpora, by default 'corpora'
+    latest_only : bool, optional
+        If true yield only the latest version of each corpus, by default True
+    include_corpora : List[str], optional
+        A list of corpus_ids that are to be included. If this argument is
+        specified, all corpora not in this list will be excluded.
+    exclude_corpora : List[str], optional
+        A list of corpus_ids that are excluded.
+
+    Yields
+    -------
+    Path
+        Path to a corpus directory
+    """
+    corpora_dir = Path(corpora_dir)
+    for corpus_dir in corpora_dir.iterdir():
+        if (
+            not corpus_dir.is_dir()
+            or corpus_dir.name in exclude_corpora
+            or (len(include_corpora) > 0 and corpus_dir.name not in include_corpora)
+        ):
+            continue
+
+        versions = []
+        for version_dir in corpus_dir.iterdir():
+            if version_dir.is_dir() and re.match("\d+\.\d+\.\d+", version_dir.name):
+                versions.append(version_dir)
+        versions = sorted(versions)
+        if len(versions) == 0:
+            continue
+        elif latest_only:
+            yield versions[-1]
+        else:
+            for version in versions:
+                yield version
+
+
+def generate_index(corpus_dir: Union[Path, str]) -> None:
+    """Generate the index for a given corpus directory"""
+    index_file = Path(corpus_dir) / "src" / "index.py"
+    if index_file.exists():
+        print("Running", index_file)
+        subprocess.run(["python", index_file.resolve()])
+
+
+def generate_indices(**kwargs) -> None:
+    """Generate indices of all corpora. Optional keyword arguments are passed
+    to iter_corpus_dir."""
+    for corpus_dir in iter_corpus_dir("corpora", **kwargs):
+        generate_index(corpus_dir)
+
+
+if __name__ == "__main__":
+    corpora = [
+        # 'boehme-altdeutsches-liederbuch',
+        "boehme-volksthumliche-lieder"
+    ]
+    generate_indices(include_corpora=corpora)
